206fc6cc60c78f8e5da642ed44164aeda278d2aa
laneyhui <laneyhui@outlook.com>
2017-01-09 17:01:40 +0800

    Update favlist.vue

--- app/index/component/favlist.vue
+++ app/index/component/favlist.vue
@@ -1,19 +1,26 @@
 <template>
-	<div v-for="n in 10">div</div>
+    <div v-for="n in 10">div</div>
 </template>
 
 <script>
-	export default {
-		data () {
-		return {
-			msg: 'Hello World!'
-		}
-	}
-	}
+    export default {
+        data () {
+            return {
+                msg: 'Hello World!'
+            }
+        }
+    }
 </script>
 
 <style>
-	html{
-		background:red;
-	}
+    /*@mixin rounded-corners{*/
+        /*border-radius:5px;*/
+    /*}*/
+    /*html{*/
+        /*background: yellow;*/
+    /*}*/
+    /*div{*/
+        /*border:2px solid #ffe87c;*/
+        /*@include rounded-corners;*/
+    /*}*/
 </style>
